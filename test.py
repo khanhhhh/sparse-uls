5b8049e5337b1f4a468b587f78b9c8cf86df5360
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-11-08 23:05:28 +0800

    fixed

--- test.py
+++ test.py
@@ -6,14 +6,16 @@ from sparse_uls.uls import solve
 
 np.random.seed(1234)
 
-def draw_hist(x: np.ndarray, title: str = "norm"):
+def draw_hist(x: np.ndarray, title: str = "norm", draw: bool=False):
     hist, edge = np.histogram(x, bins=101, range=[-0.1, +0.1])
     center = np.array([0.5 * (edge[i] + edge[i + 1]) for i in range(len(hist))])
-    plt.title(title)
-    plt.xlabel("values")
-    plt.ylabel("occurrences")
-    plt.bar(center, hist, width=(center[1] - center[0]))
-    plt.show()
+    print(f"middle [{edge[int(len(hist)/2)]}, {edge[1+int(len(hist)/2)]}] occurences: {hist[int(len(hist)/2)]}")
+    if draw:
+        plt.title(title)
+        plt.xlabel("values")
+        plt.ylabel("occurrences")
+        plt.bar(center, hist, width=(center[1] - center[0]))
+        plt.show()
 
 def norm_p(x: np.ndarray, p: float=2.0) -> float:
     return np.sum(np.abs(x)**p)**(1/p)
