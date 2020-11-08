0300480ae0fba30ba6a5806af886d18aab0bb274
Nguyen Ngoc `Khanh` <nguyenngockhanh.pbc@gmail.com>
2020-11-09 02:09:17 +0800

    matlab

--- test.m
+++ test.m
@@ -1,3 +1,11 @@
-A = rand(2, 3);
-b = rand(2);
-uls(A, b);
+n = 1000;
+m = 200;
+
+A = rand(m, n);
+b = rand(m, 1);
+tic;
+x = sparse_uls(A, b);
+toc;
+fprintf("constraint: %f\n", sum(abs(A*x-b)));
+fprintf("norm: %f\n", sum(abs(x)));
+fprintf("sparsity: %f\n", sum(x ==0) / n);
