b93469c1011e634c6a24e94ed5eba0c0014681ad
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-11-13 05:29:50 +0800

    added test octave

---
+++ test_octave.py
@@ -0,0 +1,49 @@
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+from oct2py import octave
+
+from sparse_uls.uls import solve
+
+octave.eval("pkg load optim")
+octave.addpath("./")
+
+
+
+np.random.seed(1234)
+
+def draw_hist(x: np.ndarray, title: str = "norm", draw: bool=False):
+    hist, edge = np.histogram(x, bins=101, range=[-0.1, +0.1])
+    center = np.array([0.5 * (edge[i] + edge[i + 1]) for i in range(len(hist))])
+    print(f"middle [{edge[int(len(hist)/2)]}, {edge[1+int(len(hist)/2)]}] occurences: {hist[int(len(hist)/2)]}")
+    if draw:
+        plt.title(title)
+        plt.xlabel("values")
+        plt.ylabel("occurrences")
+        plt.bar(center, hist, width=(center[1] - center[0]))
+        plt.show()
+
+def norm_p(x: np.ndarray, p: float=2.0) -> float:
+    return np.sum(np.abs(x)**p)**(1/p)
+
+n = 1000
+m = 200
+A = np.random.random(size=(m, n)).astype(dtype=np.float64)
+b = np.random.random(size=(m)).astype(dtype=np.float64)
+
+for p in [1, 1.001, 1.5, 2.0]:
+    t0 = time.time()
+    if p == 1:
+        b_ = b.reshape((b.shape[0], 1))
+        x_ = octave.sparse_uls(A, b_)
+        x = x_.reshape((x_.shape[0],))
+    else:
+        x = solve(A, b, p)
+    t1 = time.time()
+    print(f"L^{p} time: {t1-t0}")
+    print(f"\tconstraints: {np.max(np.abs(A @ x - b))}")
+    print(f"\tL^p norm: {norm_p(x, p)}")
+    draw_hist(x, f"L^{p} norm", draw=True)
+
+pass
