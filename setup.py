2ca4189b92507185494d838d526fae946c25f363
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-11-15 21:40:56 +0800

    release 5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sparse_uls",
-    version="5.1",
+    version="5.2",
     author="Nguyen Ngoc Khanh",
     author_email="nguyenngockhanh.pbc@gmail.com",
     description="optimize norm with underdetermined system equality constraint",
